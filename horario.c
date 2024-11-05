bbd048bc2d47bd91af9175931f542638875dce8a
Keitiely Silva <162761003+keitiely@users.noreply.github.com>
2024-11-05 07:00:28 -0300

    Create horario.c

---
+++ horario.c
@@ -0,0 +1,20 @@
+/*5- Defina um tipo de estrutura para armazenar um horário composto de hora,
+minutos e segundos. Crie e inicialize uma variável desse tipo e,
+em seguida, mostre seu valor no vídeo usando o formato "99:99:99".*/
+
+#include <stdio.h>
+
+typedef struct{
+    int hora;
+    int minutos;
+    int segundos;
+}horário;
+
+int main(){
+    
+  horário hours = {17, 30, 46 };
+  
+  printf("\n\tHora definida: %d:%d:%d", hours.hora, hours.minutos, hours.segundos);
+
+    return 0;
+}
