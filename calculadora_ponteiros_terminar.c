65a0d60f641b4784f189ab3e315c74ba2fa0a97f
Keitiely Silva <162761003+keitiely@users.noreply.github.com>
2024-11-07 15:19:13 -0300

    Update calculadora_ponteiros.c

--- calculadora_ponteiros_terminar.c
+++ calculadora_ponteiros_terminar.c
@@ -14,25 +14,42 @@ float mult(int a, int b){
     return a * b;
 };
 float divi(int a, int b){
+    if(b!=0)
     return a / b;
+    else
+    printf("\nNao existe divisao por zero");
+    return 0;
 };
 
 
  int main() {
+    int num1, num2, i;
     static float (*c[])(int num1, int num2) = {soma, sub, mult, divi};
+    
     printf("\n\tCalculadora Polimorfica com ponteiros");
-    printf("\nDigite qual operacao fazer: ");
-    int i;
+   
     do{
-        printf("\n0. Somar ");
-        printf("\n1. Subtrair ");
-        printf("\n2. Multiplicar ");
-        printf("\n3. Dividir ");
-        printf("\n4. Fechar ");
+        printf("\n\n  0. Somar ");
+        printf("\n  1. Subtrair ");
+        printf("\n  2. Multiplicar ");
+        printf("\n  3. Dividir ");
+        printf("\n  4. Fechar ");
+        printf("\n\n Digite a opcao: ");
+        scanf("\n %d", &i);
+    
         
-        
-        if (i>=0 && i<=3)
-            c[i](num1, num2);
+        if(i>=0 && i<=3){
+            printf("\n Digite o primeiro numero: ");
+            scanf("%d", &num1);
+            printf(" Digite o segundo numero: ");
+            scanf("%d", &num2);
+            printf("\n Resultado: %.2f", c[i](num1, num2));
+        } else if(i != 4){
+            printf("\n Numero invalido, tente novamente! ");
+        }
+     
+
     }while(i != 4);
+    printf("\n Calculadora encerrada!");
  
 }
